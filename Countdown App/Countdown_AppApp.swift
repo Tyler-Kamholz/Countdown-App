75de3c647440ac3d9e276c0492eba5bc8065a585
tylerkamholz <kamholz.tyler@gmail.com>
2024-01-22 14:33:26 -0600

    Initial Commit

---
+++ Countdown App/Countdown_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Countdown_AppApp.swift
+//  Countdown App
+//
+//  Created by Tyler  Kamholz on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Countdown_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
